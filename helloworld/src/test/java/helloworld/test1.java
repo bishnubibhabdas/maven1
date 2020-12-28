7f3b0803bb67bf93743b870c39f0170bd3a8961b
BI20171657 <BI20171657@APL-7CRD403>
2020-12-28 15:52:07 +0530

    QA stage commited

---
+++ helloworld/src/test/java/helloworld/test1.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.Test;
+
+public class test1 {
+	@Test
+	void testcase()
+	{
+	Assertions.assertEquals("hello","hello");
+	}
+
+}
